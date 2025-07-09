cfe9315c24d2ac53f91ec811eaf18ee09a76df6f
Mirza Sarim Baig <sarimbaig321@gmail.com>
2025-07-10 00:26:21 +0500

    changes

--- src/components/LogoGenerator/LogoGenerator.jsx
+++ src/components/LogoGenerator/LogoGenerator.jsx
@@ -23,7 +23,7 @@ const LogoGenerator = () => {
 
   const generateLogo = async (prompt) => {
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+      "",
       {
         method: "POST",
         headers: {
